d5001c3f804ac1444e96b4baafabe648742503ab
Yogi Dharma <yogidharma21@gmail.com>
2026-05-01 13:32:26 +0700

    fix: correct CSV parsing

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,34 +1,16 @@
 import mlflow
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
 
 mlflow.set_experiment("Credit_Card_Fraud_Detection")
 
-df = pd.read_csv("credit_card_fraud_preprocessing.csv")
+df = pd.read_csv("credit_card_fraud_preprocessing.csv", sep=",")
 
-print("Kolom dataset:", df.columns)
+print("Kolom:", df.columns)
 
 target_col = "Class"
 
 if target_col not in df.columns:
-    raise ValueError(f"Kolom '{target_col}' tidak ditemukan!")
+    raise ValueError(f"Kolom '{target_col}' tidak ditemukan! Kolom: {df.columns}")
 
 X = df.drop(target_col, axis=1)
 y = df[target_col]
-
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=42
-)
-
-model = RandomForestClassifier()
-model.fit(X_train, y_train)
-
-y_pred = model.predict(X_test)
-acc = accuracy_score(y_test, y_pred)
-
-mlflow.log_param("model", "RandomForest")
-mlflow.log_metric("accuracy", acc)
-
-print(f"Accuracy: {acc}")
