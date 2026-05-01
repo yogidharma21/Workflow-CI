7e00b3ddadc055c9fb85db207c6d7ce45857d009
Yogi Dharma <yogidharma21@gmail.com>
2026-05-01 13:30:46 +0700

    fix: correct target column name

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -8,12 +8,20 @@ mlflow.set_experiment("Credit_Card_Fraud_Detection")
 
 df = pd.read_csv("credit_card_fraud_preprocessing.csv")
 
-X = df.drop("Class", axis=1)
-y = df["Class"]
+print("Kolom dataset:", df.columns)
+
+target_col = "Class"
+
+if target_col not in df.columns:
+    raise ValueError(f"Kolom '{target_col}' tidak ditemukan!")
+
+X = df.drop(target_col, axis=1)
+y = df[target_col]
 
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
+
 model = RandomForestClassifier()
 model.fit(X_train, y_train)
 
